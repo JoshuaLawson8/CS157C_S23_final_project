e824644debf84868f5ef356c7a73782a4ce7499e
Nimesh Nischal <n.nischal.1994@gmail.com>
2023-04-20 20:00:25 -0700

    New backend service

---
+++ backend/index.js
@@ -0,0 +1,105 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const ReviewModel = require('./ReviewModel');
+require('dotenv').config();
+
+
+mongoose.connect(`${process.env.MONGO_URL || 'mongodb://localhost:27017'}/yelp`, { useNewUrlParser: true });
+
+const mdb = mongoose.connection;
+
+mdb.on('error', (err) => {
+    console.error('Mongoose connection error: ', err)
+})
+mdb.on('connected', () => {
+    console.log('Mongoose connected')
+});
+
+const app = express()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+const port = 8080
+
+app.get('/', async (req, res) => {
+    try {
+        const review = await getOneReview();
+        res.send(review);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err.message);
+    }
+});
+
+app.get('/:id', async (req, res) => {
+    try {
+        const review = await getByID(req.params.id);
+        if (!review) {
+            res.status(404).send('Review not found');
+            return;
+        }
+        res.send(review);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err.message);
+    }
+});
+
+
+app.post('/', async (req, res) => {
+    try {
+        console.log(req.body);
+        const review = await createReview(req.body);
+        res.status(201).send(review);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err.message);
+    }
+});
+
+app.delete('/:id', async (req, res) => {
+    try {
+        const review = await deleteReview(req.params.id);
+        if (!review) {
+            res.status(404).send('Review not found');
+            return;
+        }
+        res.status(200).send(review);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err.message);
+    }
+});
+
+
+const getOneReview = async () => {
+    return ReviewModel.findOne({});
+}
+
+const createReview = async (review) => {
+    const newReview = new ReviewModel(review);
+    return newReview.save();
+}
+
+const deleteReview = async (id) => {
+    return ReviewModel.findByIdAndDelete(id);
+}
+
+const getByID = async (id) => {
+    return ReviewModel.findById(id);
+}
+
+const func5 = async () => {
+    return ReviewModel.findOne({});
+}
+
+
+app.listen(port, () => {
+    console.log(`Backend listening on port ${port}`)
+})
+
+
+
+
